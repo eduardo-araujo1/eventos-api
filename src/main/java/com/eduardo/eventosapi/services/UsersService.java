3b93452e365b0237bb75b6b6ec209800407c3511
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-12-02 19:26:16 -0300

    Método busca por id adicionado

--- src/main/java/com/eduardo/eventosapi/services/UsersService.java
+++ src/main/java/com/eduardo/eventosapi/services/UsersService.java
@@ -4,6 +4,7 @@ import com.eduardo.eventosapi.entities.Users;
 import com.eduardo.eventosapi.repositories.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +12,13 @@ public class UsersService {
 
     private final UsersRepository repository;
 
-
+    @Transactional
     public Users create(Users users) {
         return repository.save(users);
+    }
+    @Transactional(readOnly = true)
+    public Users findById(Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario id não encontrado"));
     }
 }
--- src/main/java/com/eduardo/eventosapi/web/controller/UsersController.java
+++ src/main/java/com/eduardo/eventosapi/web/controller/UsersController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +22,11 @@ public class UsersController {
     public ResponseEntity<UsersResponseDTO> create (@Valid @RequestBody UsersRequestDTO dto){
        Users createUser = service.create(UsersMapper.toUser(dto));
        return ResponseEntity.status(HttpStatus.CREATED).body(UsersMapper.toDto(createUser));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsersResponseDTO> findById (@PathVariable Long id){
+        Users users = service.findById(id);
+        return ResponseEntity.ok(UsersMapper.toDto(users));
     }
 }
