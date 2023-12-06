b0fb842007582f1f53bcb344d37cb7ea1647e330
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-12-05 23:03:36 -0300

    testes de busca por ID

--- src/test/java/com/eduardo/eventosapi/domain/UsersServiceTest.java
+++ src/test/java/com/eduardo/eventosapi/domain/UsersServiceTest.java
@@ -1,6 +1,8 @@
 package com.eduardo.eventosapi.domain;
 
 import com.eduardo.eventosapi.entities.Users;
+import com.eduardo.eventosapi.exception.EmailUniqueViolation;
+import com.eduardo.eventosapi.exception.EntityNotFoundException;
 import com.eduardo.eventosapi.repositories.UsersRepository;
 import com.eduardo.eventosapi.services.UsersService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,12 +11,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 public class UsersServiceTest {
@@ -40,11 +45,39 @@ public class UsersServiceTest {
 
 
     @Test
-    public void createUser_WithValidData_ReturnsUser(){
+    public void createUser_WithValidData_ReturnsUser() {
         when(repository.save(USER)).thenReturn(USER);
 
         Users sut = userService.create(USER);
 
         assertThat(sut).isEqualTo(USER);
     }
+
+    @Test
+    public void createUser_WithInvalidData_ThrowsException() {
+        when(repository.save(INVALID_USER)).thenThrow(EmailUniqueViolation.class);
+
+        assertThatThrownBy(() -> userService.create(INVALID_USER)).isInstanceOf(EmailUniqueViolation.class);
+    }
+
+    @Test
+    public void getUser_ByExistingId_ReturnsUser() {
+        when(repository.findById(1L)).thenReturn(Optional.of(USER));
+
+        Optional<Users> sut = Optional.ofNullable(userService.findById(1L));
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(USER);
+    }
+
+    @Test
+    public void getUser_ByUnexistingId_ThrowsException() {
+        when(repository.findById(10L)).thenThrow(EntityNotFoundException.class);
+
+        assertThatThrownBy(() -> userService.findById(10L)).isInstanceOf(EntityNotFoundException.class);
+    }
+
+
+
+
 }
